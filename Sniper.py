51eb47638a7f3f38661d10897eb721da7a80d286
TheBeeMC <thebeemcnetwork@gmail.com>
2018-08-27 23:46:02 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -16,14 +16,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!deleteme'):
-        msg = await client.send_message(message.channel, 'I will delete myself now...')
+    if message.content.startswith('Fuck'):
         await client.delete_message(msg)
-
-@client.event
-async def on_message_delete(message):
-    fmt = '{0.author.name} has deleted the message:\n{0.content}'
-    await client.send_message(message.channel, fmt.format(message))
 
 @client.event
 async def on_ready():
