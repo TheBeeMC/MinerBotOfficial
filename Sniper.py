138e3b1e00a0a09a73dc637dc5e8b7e854ba3861
TheBeeMC <thebeemcnetwork@gmail.com>
2018-08-30 17:47:34 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -29,23 +29,10 @@ async def on_message(message):
                 
                 
                 
-        if message.content.startswith('$guess'):
-            await message.channel.send('Guess a number between 1 and 10.')
-
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
-
-            answer = random.randint(1, 10)
-
-            try:
-                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send('Sorry, you took too long it was {}.'.format(answer))
-
-            if int(guess.content) == answer:
-                await message.channel.send('You are right!')
-            else:
-                await message.channel.send('Oops. It is actually {}.'.format(answer))
+@cool.command(name='$help')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('***Help***')
       
                         
                                  
