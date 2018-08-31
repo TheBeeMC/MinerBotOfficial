e43bdfbd5da275476f24260c34cfbd6262b43086
TheBeeMC <thebeemcnetwork@gmail.com>
2018-08-31 11:17:13 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -19,25 +19,14 @@ async def on_message(message):
             await bot.send_message(message.channel, embed=embed)
 
     if message.content.startswith('+help'):
-        embed=discord.Embed(title="Miner Bot Help", description="+invite: Prints an invite link for the bot.", color=0x1a94f0)
+        embed=discord.Embed(title="***Payment Infos***", description="Bot Cost: 5$", color=0x1a94f0)
         embed.set_author(name='Commands', icon_url="")
-        embed.add_field(name="+help: Brings up list of commands", value="+info: Prints info about the bot.", inline=True)
-        embed.set_footer(text='Miner Bot™ @ coded by Captain#2713')
+        embed.add_field(name="Info: You will pay 5$ PayPal to @Captain once payment goes in with Friends & Family ONLY then you will invite me to your server and give me Administrator so I can add the bot itself. This is the only way for you to get the discord bot after purchase. Remember this is the only way to protect it from getting it leaked.s", value="+info: Prints info about the bot.", inline=True)
+        embed.set_footer(text='Why chose Miner Bot? Because it is fun and I will be adding better things to it such as giveaways and polls and ping ms and more? Even get everyone active!!!')
         await bot.send_message(message.channel, embed=embed)
 
-    if message.content.startswith('+Loltest'):
-        await bot.send_message(message.channel, "***Payment Infos***")
-        await bot.send_message(message.channel, "")    
-        await bot.send_message(message.channel, "Bot Cost: 5$ ")
-        await bot.send_message(message.channel, "Refunds: Not allowed")    
-        await bot.send_message(message.channel, "Info: You will pay 5$ PayPal to @Captain once payment goes in with Friends & Family ONLY then you will invite me to your server and give me Administrator so I can add the bot itself. This is the only way for you to get the discord bot after purchase. Remember this is the only way to protect it from getting it leaked.")
-        await bot.send_message(message.channel, "Why chose Miner Bot? Because it is fun and I will be adding better things to it such as giveaways and polls and ping ms and more? Even get everyone active!!!")    
-        await bot.send_message(message.channel, "***Payment Infos***")
-        await bot.send_message(message.channel, "***Payment Infos***")    
-        await bot.send_message(message.channel, "***Payment Infos***")
-        await bot.send_message(message.channel, "***Payment Infos***")    
-        await bot.send_message(message.channel, "***Payment Infos***")
-        await bot.send_message(message.channel, "***Payment Infos***")            
+    if message.content.startswith('FUCK'):
+        await bot.send_message(message.channel, "Don't swear")
         print('Bot has been added to new server')
 
     if message.content.startswith('+info'):
