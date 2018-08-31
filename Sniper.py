49f9df8349d8556cad80b04b19fa2cc5e300b135
TheBeeMC <thebeemcnetwork@gmail.com>
2018-08-31 11:26:07 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -7,29 +7,37 @@ import os
 import subprocess
 import logging
 
-command_prefix='$'
+command_prefix='/'
 bot = commands.Bot(command_prefix)
 description = 'Miner Bot™ @ coded by Captain#2713'
  
 @bot.event
 async def on_message(message):
-    if message.content.startswith('$status'):
+    if message.content.startswith('/status'):
             embed = discord.Embed(title="Miner Bot Status: 🔵 Online", description="Everything is healthy", colour=0x1a94f0)
             embed.set_footer(text="Miner Bot™ @ coded by Captain#2713")
             await bot.send_message(message.channel, embed=embed)
 
-    if message.content.startswith('+help'):
+    if message.content.startswith('/help'):
         embed=discord.Embed(title="***Miner Bot Help***", description="Bot Cost: 5$", color=0x1a94f0)
         embed.set_author(name='Commands', icon_url="")
-        embed.add_field(name="***Coming soon***", value="$status to check if Miner is online", inline=True)
+        embed.add_field(name="***Coming soon***", value="/status to check if Miner is online", inline=True)
         embed.set_footer(text='Miner Bot Status: 🔵 Online')
         await bot.send_message(message.channel, embed=embed)
+        
+        
+    if message.content.startswith('lolgame'):
+        embed=discord.Embed(title="***Miner Bot is now back online***", description="🔵", color=0x1a94f0)
+        embed.set_author(name='Service: Healthy 🔵', icon_url="")
+        embed.add_field(name="Thank you for your patience", value="Bot is now back online!", inline=True)
+        embed.set_footer(text='Posted by Captain#2713')
+        await bot.send_message(message.channel, embed=embed)        
 
       
     if message.content.startswith('fuck'):
         await bot.send_message(message.channel, "Try not swearing next time ok? 😡")      
 
-    if message.content.startswith('+info'):
+    if message.content.startswith('/info'):
         await bot.send_message(message.channel, "Miner Bot™ @ coded by Captain#2713")
 
 async def status_task():
