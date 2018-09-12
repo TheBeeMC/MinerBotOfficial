f82062591c68339b348e58ab61b9aa1dbe97785a
TheBeeMC <thebeemcnetwork@gmail.com>
2018-09-13 04:27:57 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix='/')
 async def on_message(message):
     if message.content.startswith('/status'):
             await bot.delete_message(message)
-            embed = discord.Embed(title="#name-dropping-alert", description="The name `Fail` is dropping and has received no queries on our availability checker!", colour=0x1a94f0)
+            embed = discord.Embed(title="#name-dropping-alert", description="The name `Jeans` is dropping and has received no queries on our Check Availability!", colour=0x1a94f0)
             embed.set_footer(text="Generated From: This is a automatic message")
             await bot.send_message(message.channel, embed=embed)
 
