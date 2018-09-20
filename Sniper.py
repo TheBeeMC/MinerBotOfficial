7e23c09dbf0619f03f7efa7e020494a59fb0fd18
TheBeeMC <thebeemcnetwork@gmail.com>
2018-09-21 05:22:45 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -28,12 +28,7 @@ async def on_message(message):
                 
 
                 
-@client.event
-    async def on_member_join(self, member):
-        guild = member.guild
-        if guild.system_channel is not None:
-            to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
-            await guild.system_channel.send(to_send)
+
 
  
       
