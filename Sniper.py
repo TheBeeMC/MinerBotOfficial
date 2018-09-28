d9ed00be8ae960f2f3d6602e580776dece16927d
TheBeeMC <thebeemcnetwork@gmail.com>
2018-09-29 04:29:30 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -42,11 +42,6 @@ async def on_ready():
     print('------')
     print('INFO')
     print('------')
-    print('Logged in as: ' + bot.user.name + ', ' + bot.user.id)
-    print('------')
-    print('Running discord.py v' + discord.__version__)
-    bot.loop.create_task(status_task())
-
     await bot.change_presence(game=discord.Game(name="https://namemc.com/minecraft-names?length_op=&length=3&lang=en&searches=100"))
     
 bot.run(os.getenv('TOKEN'))
