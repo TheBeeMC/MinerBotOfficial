2149ce436fe735e18554c0dad789d5c19c621eb4
TheBeeMC <thebeemcnetwork@gmail.com>
2018-09-29 04:27:29 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -1,47 +1,52 @@
+# sniper.py
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
+import random
 import asyncio
 import os
-import time
+import subprocess
+import logging
 
-Client = discord.Client()
-client = commands.Bot(command_prefix = "?")
-
-chat_filter = ["FUCK", "BITCH", "SHIT", "FAG", "FAGGOT", "FUCKER", "ASS", "ASSHOLE", "NIGGA", "NIGER", "NIGA", "CUNT", "L", "LOSER", "BAD", "NIGGER", "FUCKK", "LMAO"]
-bypass_list = []
-
-
-
-@client.event
-async def on_message(message):
-    contents = message.content.split(" ") #contents is a list type
-    for word in contents:
-        if word.upper() in chat_filter:
-            if not message.author.id in bypass_list:
-                try:
-                    await client.delete_message(message)
-                    await client.send_message(message.channel, "**Bot**: Hey no swearing out here ok?")
-                    await client.send_message(message.channel, "**You**: https://media1.tenor.com/images/b531fcefb582e156b994a6765ee47dc7/tenor.gif?itemid=5658414")
-                except discord.errors.NotFound:
-                    return
-                
-                
-                
-            
-
-                
-
-
+command_prefix='+'
+bot = commands.Bot(command_prefix)
+description = 'sniper.py, coded by unpredictable'
  
-      
-@client.event
+@bot.event
+async def on_message(message):
+    if message.content.startswith('+snipeadd'):
+            embed = discord.Embed(title="Let's walk you through the process of queueing a snipe.", description="DM the bot with 'beginqueue' to queue your snipe.", colour=0x1a94f0)
+            embed.set_author(name="Thanks for choosing sniper.py!", icon_url="")
+            embed.add_field(name="Enjoy your new name!", value="-trinity and iliyan")
+            embed.set_footer(text="sniper.py™ © coded by unpredictable")
+            await bot.send_message(message.channel, embed=embed)
+
+    if message.content.startswith('+help'):
+        embed=discord.Embed(title="+snipeadd: queues a snipe", description="+invite: Prints an invite link for the bot.", color=0x1a94f0)
+        embed.set_author(name='Commands', icon_url="")
+        embed.add_field(name="+help: Brings up list of commands", value="+info: Prints info about the bot.", inline=True)
+        embed.set_footer(text='coded by unpredictable')
+        await bot.send_message(message.channel, embed=embed)
+
+    if message.content.startswith('+invite'):
+        await bot.send_message(message.channel, "**Here's the invite for the bot:** INVITE")
+        print('Bot has been added to new server')
+
+    if message.content.startswith('+info'):
+        await bot.send_message(message.channel, "Coded by unpredictable. Bot hosted on Heroku, coded with python3 and Discord.PY.")
+
+
+
+@bot.event
 async def on_ready():
-    print('No Swearing Bot #1')
+    print('sniper.py coded by unpredictable')
     print('------')
     print('INFO')
     print('------')
-    print('Logged in as: ' + client.user.name + ', ' + client.user.id)
-    await client.change_presence(game=discord.Game(name="#1 No Swearing Bot"))
-        
-client.run(os.getenv('TOKEN'))
+    print('Logged in as: ' + bot.user.name + ', ' + bot.user.id)
+    print('------')
+    print('Running discord.py v' + discord.__version__)
+    bot.loop.create_task(status_task())
+
+    await bot.change_presence(game=discord.Game(name="Checking "))
+    
+bot.run(os.getenv('TOKEN'))
