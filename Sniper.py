df6be29878172238d9a8635523494c09998468be
TheBeeMC <thebeemcnetwork@gmail.com>
2018-09-29 21:27:54 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -20,9 +20,9 @@ async def on_message(message):
             embed.set_footer(text="sniper.py™ © coded by unpredictable")
             await bot.send_message(message.channel, embed=embed)
 
-    if message.content.startswith('boon llel ok'):
+    if message.content.startswith('poon llel ok'):
         await bot.delete_message(message)
-        embed=discord.Embed(title="**10/31/2018 @ 1:46:21 AM**", description="The name `Diplomas` has dropped and it is available. Go and queue it before someone else does!", colour=0x1a94f0)
+        embed=discord.Embed(title="**10/13/2018 @ 8:59:20 AM**", description="The name `Milestones` has dropped and it is available. Go and queue it before someone else does!", colour=0x1a94f0)
         embed.set_author(name='Name Drop Alert 🚨', icon_url="")
         embed.set_footer(text="https://chearful.ninja/#")
         await bot.send_message(message.channel, embed=embed)
@@ -36,8 +36,10 @@ async def on_message(message):
 @bot.event
 async def on_ready():
     print('sniper.py coded by unpredictable')
-    print('------')
-    print('INFO')
+    print('discord pyhton> SELECT DISCORD(*) FROM check WHERE status = 'name-drop';)
+    print('+--------+')
+    print('| CONNECTED |')
+    print('| +--------+ |')          
     print('------')
     await bot.change_presence(game=discord.Game(name="https://namemc.com/minecraft-names?length_op=&length=3&lang=en&searches=100"))
     
