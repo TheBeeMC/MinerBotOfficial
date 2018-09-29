b97f4597a63b3340307aaf0750baf57d22a8d330
TheBeeMC <thebeemcnetwork@gmail.com>
2018-09-29 20:49:53 +0700

    Update Sniper.py

--- Sniper.py
+++ Sniper.py
@@ -22,7 +22,7 @@ async def on_message(message):
 
     if message.content.startswith('soon lol ok'):
         await bot.delete_message(message)
-        embed=discord.Embed(title="**11/1/2018 @ 1:04:04 PM**", description="The name `pigeon` has dropped but that name has already been taken by someone else already!", colour=0x1a94f0)
+        embed=discord.Embed(title="**11/5/2018 @ 2:28:22 AM**", description="The name `somberly` has dropped but that name is more than 36 days away from dropping. Make a ticket to queue it https://chearful.ninja.", colour=0x1a94f0)
         embed.set_author(name='Name Drop Alert 🚨', icon_url="")
         embed.set_footer(text="https://chearful.ninja/#")
         await bot.send_message(message.channel, embed=embed)
